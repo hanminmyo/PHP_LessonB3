8068bbd1ed68e38f6c78a9373c1df98e1d18020f
hanminmyo <myominnhann.mmh@gmail.com>
2024-09-03 22:16:49 +0700

    PHP lesson number and variable,string.php

--- index.php
+++ index.php
@@ -2,3 +2,23 @@
     echo "Hello World!";
     echo "<h1>Super Friday</h1>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>This is paragraph</h1>";
+    ?>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos architecto, natus repudiandae maiores saepe soluta deleniti earum? Iusto unde quibusdam commodi natus earum! Nisi, exercitationem? A nulla unde ea id perferendis, est necessitatibus tempora quis error exercitationem optio beatae omnis minima illum quidem minus quia assumenda laboriosam nihil accusamus! Mollitia?</p>
+
+    <script>
+        document.write('<h1>I am javascript</h1>');
+    </script>
+    
+</body>
+</html>
