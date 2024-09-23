43356e7340c17dd8056255505d0dd23ef73d34d5
hanminmyo <myominnhann.mmh@gmail.com>
2024-09-23 10:42:03 +0700

    php template tutorials

---
+++ template/overview.php
@@ -0,0 +1,10 @@
+<?php
+    include "nav_sidebar.php";
+?>
+<div class="container-fluid">
+    <h1>This is overview page.</h1>
+</div>
+
+<?php
+    include "footer.php";
+?>
---
+++ template/status.php
@@ -0,0 +1,10 @@
+<?php 
+    include "nav_sidebar.php"; 
+?>
+<div class="container-fluid">
+    <h1>This is status page</h1>
+</div>
+
+<?php
+    include "footer.php";
+?>
